fd9fd61025ae369b2842024262a24f6d45036332
anshasiq <ahmadanshasi123@hotmail.com>
2023-10-18 19:45:43 +0000

    final

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -3,19 +3,65 @@ package com.example.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String userNickname = preferences.getString(SettingsPage.KEY, "No nickname");
+        TextView d = (TextView) findViewById(R.id.textView6);
+        d.setText(userNickname);
+        System.out.println(userNickname);
         Button alltasks = (Button) findViewById(R.id.button);
         Button addtasks = (Button) findViewById(R.id.button2);
+
+        Button gowork = (Button) findViewById(R.id.button7);
+        Button study = (Button) findViewById(R.id.button8);
+        Button todo = (Button) findViewById(R.id.button9);
+        Button setting = (Button) findViewById(R.id.button6);
+
+
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SettingsPage.class);
+                startActivity(intent);
+            }
+        });
+        gowork.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TaskDetailPage.class);
+                intent.putExtra("key", "gowork");
+                startActivity(intent);
+            }
+        });
+        study.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TaskDetailPage.class);
+                intent.putExtra("key", "study");
+                startActivity(intent);
+            }
+        });
+        todo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TaskDetailPage.class);
+                intent.putExtra("key", "todo");
+                startActivity(intent);
+            }
+        });
         alltasks.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
---
+++ screenshots/ReadMe.md
@@ -0,0 +1,18 @@
+# for lab 26
+### i add 3 activity Home , add task ,all task
+
+
+# for Lab 27 
+### i add everything as recommended 
+### from the settings you can add a username and see it in the home page
+### from study , todo , go work. you can open the detail page
+# all images
+![image description](Screenshot_20231018-222212_taskmaster.jpg)
+![image description](Screenshot_20231018-222217_taskmaster.jpg)
+![image description](Screenshot_20231018-222220_taskmaster.jpg)
+![image description](Screenshot_20231018-222225_taskmaster.jpg)
+![image description](Screenshot_20231018-222233_taskmaster.jpg)
+![image description](Screenshot_20231018-222949_taskmaster.jpg)
+![image description](Screenshot_20231018-223215_taskmaster.jpg)
+
+
