02fccacac8847d0366fccd3ae489c1ae4fedc5ce
anshasiq <ahmadanshasi123@hotmail.com>
2023-10-25 20:59:41 +0000

    class

---
+++ app/src/main/java/com/example/taskmaster/Model/Task.java
@@ -0,0 +1,35 @@
+package com.example.taskmaster.Model;
+
+public class Task {
+    String title;
+    String body;
+    String state;
+
+    public Task(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
